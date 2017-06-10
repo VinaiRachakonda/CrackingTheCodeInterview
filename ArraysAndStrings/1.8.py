c22a9d4e0ef4f3e0e0f8f4cd1d3bb4539ffc774a
ERHSQuizbowl <vinaichand@yahoo.com>
2017-06-10 17:25:08 -0400

    1.8

---
+++ ArraysAndStrings/1.8.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+def isSubstring(s1, s2):
+	return s2 in s1
+
+
+def isRotation(s1, s2):
+
+	if (len(s1) != len(s2)):
+		return false
+
+	ccat = s1 + s1
+
+	return isSubstring(s2, s1)
+
+
+class TestCases(unittest.TestCase):
+	def test_simple_no(self):
+		self.assertFalse(isRotation("a", "b"))
+
+	def test_simple_yes(self):
+		self.assertFalse(isRotation("ab", "ba"))
+
+	def test_complex_yes(self):
+		self.assertFalse(isRotation("erbottlewat", "waterbottle"))
+
+if __name__ == '__main__':
+    unittest.main()

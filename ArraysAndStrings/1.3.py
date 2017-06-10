a22c451f6b9f3362088ed2c1cab0256ab2f4d447
ERHSQuizbowl <vinaichand@yahoo.com>
2017-06-10 17:03:17 -0400

    did something

--- ArraysAndStrings/1.3.py
+++ ArraysAndStrings/1.3.py
@@ -1,12 +1,6 @@
-# First idea is to hash while iterating and removing if key is seen
-# Hashing would be O(1) time. Total O(n). 
-
-# From problem 1.1 the ascii array seems like a more efficient idea
-
 import unittest
 
-
-def removeDuplicates(input):
+def removeDuplicatesWithBuffer(input):
 	boolA = [False] * 256
 
 	ret = ""
@@ -14,27 +8,27 @@ def removeDuplicates(input):
 		if (not boolA[ord(input[i])]):
 			boolA[ord(input[i])] = True
 			ret = ret + input[i]
-			
+		
 	return ret
 
 class TestCases(unittest.TestCase):
 	def test_no_change(self):
-		self.assertEquals('vina', removeDuplicates('vina'))
+		self.assertEquals('vina', removeDuplicatesWithBuffer('vina'))
 
 	def test_last_letter(self):
-		self.assertEquals('vina', removeDuplicates('vinai'))
+		self.assertEquals('vina', removeDuplicatesWithBuffer('vinai'))
 
 	def test_very_long(self):
-		self.assertEquals('ban', removeDuplicates('banana'))
+		self.assertEquals('ban', removeDuplicatesWithBuffer('banana'))
 
 	def test_very_empty(self):
-		self.assertEquals('', removeDuplicates(''))
+		self.assertEquals('', removeDuplicatesWithBuffer(''))
 
 	def test_singleton(self):
-		self.assertEquals('s', removeDuplicates('s'))
+		self.assertEquals('s', removeDuplicatesWithBuffer('s'))
 
 	def test_odd_symbols(self):
-		self.assertEquals('s!&8', removeDuplicates('s!!&8&'))
+		self.assertEquals('s!&8', removeDuplicatesWithBuffer('s!!&8&'))
 
 if __name__ == '__main__':
     unittest.main()
